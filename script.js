9ddd2d85950cf0be106f5e5640c4ff83386cd9a6
sayeed <sayeed0209@gamil.com>
2020-07-14 10:34:31 +0200

    add functionality to js

--- script.js
+++ script.js
@@ -4,15 +4,27 @@ $(document).ready(function() {
   
   function loadFolders () {
     const action = "load"
+    var old_name = $("#old_name").val()
 
     $.ajax({
       url: "script.php",
       method: "POST",
-      data: {action:action},
+      data: {action:action,
+      old_name:old_name},
       success: function(data) {
         $('#table').append(data)
       }
     })
-  } 
+  }
+  $(document).on('click','#update',function(e){
+    let folderName = $(this).data('name');
+    // console.log(folderName)
+    $('#old_name').val(folderName);
+    $('#name_input').val(folderName);
+    $('#action').val('change')
+    $('#create_file').val('Update')
+    $("#change_title").text('Change Folder Name')
+    $('#exampleModal').modal('show')
+  })
 })
 
--- script.php
+++ script.php
@@ -17,8 +17,8 @@
             <th scope='col'><img src='https://image.freepik.com/free-vector/illustration-data-folder-icon_53876-6329.jpg' width='30' height='30'>$folder</th>
             <th scope='col'>$size bytes</th>
             <th scope='col'>$modified</th>
-            <td><button type='button' class='btn btn-warning'>Update</button></td>
-            <td><button type='button' class='btn btn-danger'>Delete</button></td>
+            <td><button type='button' class='btn btn-warning' id='update' data-name='folder'>Update</button></td>
+            <td><button type='button' class='btn btn-danger' id='delete'>Delete</button></td>
         </tr>";
       };
 
@@ -32,16 +32,27 @@
             <th scope='col'><img src='https://cdn3.iconfinder.com/data/icons/brands-applications/512/File-512.png' width='20' height='20'>$file</th>
             <th scope='col'>$size bytes</th>
             <th scope='col'>$modified</th>
-            <td><button type='button' class='btn btn-warning'>Update</button></td>
-            <td><button type='button' class='btn btn-danger'>Delete</button></td>
+            <td><button type='button' class='btn btn-warning' id='update' data-name='folder'>Update</button></td>
+            <td><button type='button' class='btn btn-danger' id='delete'>Delete</button></td>
         </tr>";
       }
     }
+    
   }
 
   if(isset($_POST["submit"])) {
     if($_POST["folder"]) {
       mkdir($_POST["folder"]);
       header("Location: index.php");
+    }
+  }
+
+  if($_POST["action"]=="change"){
+    if(!file_exists($_POST["folder"])){
+      rename($_POST["old_name"],$_POST["folder"]);
+      echo "Folder name changed";
+
+    }else{
+      echo 'folder already created';
     }
   }
