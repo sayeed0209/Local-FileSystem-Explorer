6956798b626cd202b2c5aca06186c158a57c2489
sayeed <sayeed0209@gamil.com>
2020-07-16 14:59:15 +0200

    small changes

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include 'upload.php';
 <body>
   <div class="container mt-3">
     <!-- Bootstrap NavBar -->
-    <nav class="navbar navbar-expand-md navbar-dark bg-info">
+    <nav class="navbar navbar-expand-md navbar-dark bg-info" id="nav">
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
